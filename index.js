bae989db1afd7df8a72b3486dd37f8d3186bb9b4
Marlon M <lonmaniti@gmail.com>
2025-05-16 11:55:17 +1200

    Init commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+app.use(express.json());
+
+app.post('/validate-token', async (req, res) => {
+    const { token } = req.body;
+
+    try {
+        const response = await axios.get('https://graph.microsoft.com/v1.0/me', {
+            headers: { Authorization: `Bearer ${token}` },
+        });
+
+        res.json({ valid: true, user: response.data });
+    } catch (error) {
+        res.status(401).json({ valid: false, error: error.response.data });
+    }
+});
+
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
